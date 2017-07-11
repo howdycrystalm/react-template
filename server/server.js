9b306f78ee0fed2c4fd86bfe53c10b78587ec0d5
taypsl <tayparsell@gmail.com>
2017-07-11 15:59:31 -0700

    updating package.json

--- server/server.js
+++ server/server.js
@@ -9,4 +9,6 @@ app.use(cors())
 
 app.use('/', express.static('js'));
 
-app.listen(process.env.PORT || 8081);  
+app.listen(process.env.PORT || 3000, function () {
+  console.log("runnin' through the 3000 with my woes");
+});  
