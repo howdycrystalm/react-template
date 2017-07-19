cf51156854bd16e40b8ac8df8c6cd014d52d84cf
taypsl <tayparsell@gmail.com>
2017-07-19 14:55:24 -0700

    fixed position bar behind search so page content hidden on scroll

--- source/js/components/Global/Search.jsx
+++ source/js/components/Global/Search.jsx
@@ -4,12 +4,14 @@ import React, { Component } from 'react';
 export default class Search extends Component {
   render() {
     return (
-      <div className='Search'>
-        <input className='Search-bar' placeholder=' Search our site' />
+      <div>
+        <div className='Search'>
+          <input className='Search-bar' placeholder=' Search our site' />
 
-        <button>
-        Search
-        </button>
+          <button>
+          Search
+          </button>
+        </div>
       </div>
     );
   }
