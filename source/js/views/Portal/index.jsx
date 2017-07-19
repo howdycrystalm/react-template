0128b2f7cb7e94b97a8c0053de44d07f61cbcac1
taypsl <tayparsell@gmail.com>
2017-07-19 15:48:15 -0700

    adding line to Square-box

--- source/js/views/Portal/index.jsx
+++ source/js/views/Portal/index.jsx
@@ -5,6 +5,12 @@ export default class Portal extends Component {
     return (
       <div className='Portal'>
         <h1>My Portal</h1>
+        <SquareBox boxTitle='Log in / Sign up'
+        		   imgSrc='../assets/icons/profile.svg'
+        />
+        <SquareBox boxTitle='Find a Court Case'
+        		   imgSrc='../assets/icons/search.svg'
+        />
       </div>
     );
   }
